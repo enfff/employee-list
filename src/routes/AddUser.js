d3cc689cdb496602da8b043887916a3a30ca4fd9
enfff <enforcetitan@gmail.com>
2022-05-13 12:26:07 +0200

    improved ui and improved readibility A
    qq
    A
    A
    A
    A
    A
    A
    A
    A
    A
    A
    A
    A
    A
    A
    A
    A
    A
    A
    A
    A
    A
    B
    B
    B
    B
    B
    B
    B
    B
    B
    B
    B
    B

--- src/routes/AddUser.js
+++ src/routes/AddUser.js
@@ -1,7 +1,5 @@
-// import { Formik, Form } from "formik";
-// import * as yup from "yup";
-import { Col, Container, InputGroup, Button, Form } from "react-bootstrap"
-import { useState } from "react"
+import { Col, Container, InputGroup, Button, Form } from "react-bootstrap";
+import { useState } from "react";
 
 const AddUser = () => {
 
@@ -20,40 +18,46 @@ const AddUser = () => {
     return (
         <Container className="justify-content-center mt-5">
             <Form noValidate validated={validated} onSubmit={handleSubmit}>
-                {/* <Row className="mb-3"> */}
-                    <Form.Group as={Col} controlId="validationCustom01">
-                        <Form.Label>Name</Form.Label>
+                <Form.Group as={Col} controlId="validationName">
+                    <Form.Label>Name</Form.Label>
+                    <Form.Control
+                        required
+                        type="text"
+                        placeholder="Thomas Milton"
+                    />
+                    <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+                    <Form.Control.Feedback type="invalid">
+                        Please choose a name.
+                    </Form.Control.Feedback>
+                </Form.Group>
+                <Form.Group as={Col} controlId="validationBio">
+                    <Form.Label>Bio</Form.Label>
+                    <Form.Control
+                        required
+                        as="textarea"
+                        type="text"
+                        placeholder="23yo Designer from San Francisco"
+                    />
+                    <Form.Control.Feedback type="invalid">
+                        Please write a bio.
+                    </Form.Control.Feedback>
+                    <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+                </Form.Group>
+                <Form.Group as={Col} controlId="validationCustomUsername">
+                    <Form.Label>Company Catchphrase</Form.Label>
+                    <InputGroup hasValidation>
+                        <InputGroup.Text id="inputGroupPrepend">@</InputGroup.Text>
                         <Form.Control
-                            required
                             type="text"
-                            placeholder="Name"
-                        />
-                        <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
-                    </Form.Group>
-                    <Form.Group as={Col} controlId="validationCustom02">
-                        <Form.Label>Company Name</Form.Label>
-                        <Form.Control
+                            placeholder="Company Catchphrase"
+                            aria-describedby="inputGroupPrepend"
                             required
-                            type="text"
-                            placeholder="Company Name"
                         />
-                        <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
-                    </Form.Group>
-                    <Form.Group as={Col} controlId="validationCustomUsername">
-                        <Form.Label>Company Catchphrase</Form.Label>
-                        <InputGroup hasValidation>
-                            <InputGroup.Text id="inputGroupPrepend">@</InputGroup.Text>
-                            <Form.Control
-                                type="text"
-                                placeholder="Company Catchphrase"
-                                aria-describedby="inputGroupPrepend"
-                                required
-                            />
-                            <Form.Control.Feedback type="invalid">
-                                Please choose a company catchprase.
-                            </Form.Control.Feedback>
-                        </InputGroup>
-                    </Form.Group>
+                        <Form.Control.Feedback type="invalid">
+                            Please choose a company catchprase.
+                        </Form.Control.Feedback>
+                    </InputGroup>
+                </Form.Group>
                 {/* </Row> */}
                 <Button block="true" type="submit" className="mt-5">Submit form</Button>
             </Form>
