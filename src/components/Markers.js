b204b14f9141f5bb3bc5517512daf0c61bdc8e47
enfff <enforcetitan@gmail.com>
2022-05-13 12:23:56 +0200

    housekeeping

--- src/components/Markers.js
+++ src/components/Markers.js
@@ -5,8 +5,7 @@ const Markers = ( {users} ) => {
 
     return (
         users.map(user => {
-            // console.log(user.geo.lat, user.geo.lng);
-            console.log(user);
+            // console.log(user);
             return <Marker key={user.id} position={user.address.geo} />
         })
     );
