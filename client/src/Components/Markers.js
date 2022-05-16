2d33517ed03e76560996c1ff46e86d940b153827
enfff <enforcetitan@gmail.com>
2022-05-17 01:00:49 +0200

    clicking on markers now shows who's there

--- client/src/Components/Markers.js
+++ client/src/Components/Markers.js
@@ -1,11 +1,24 @@
-import { Marker } from 'react-leaflet'
+import { Marker, Popup } from 'react-leaflet'
 
-
-const Markers = ( {users} ) => {
+const Markers = ({ users }) => {
 
     return (
         users.map(user => {
-            return <Marker key={user._id} position={user.geo} />
+            return (
+                <>
+                    <Marker key={user._id} position={user.geo}>
+                        <Popup>
+                            <h4>{user.name}</h4>
+                            <h6>Works for {user.company}</h6>
+                            <h6>{user.geo[0]}, {user.geo[1]}</h6>
+                        </Popup>
+                    </Marker>
+
+                </>
+            )
+
+
+
         })
     );
 }
